c51c9da928dd2f65130706d0a764db1215c3bd33
Cartercoder <calvin.carter35@gmail.com>
2020-07-04 09:41:16 -0500

    Have to fix card

---
+++ Web/Shared/TCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Web.Shared
+{
+    public class TCard
+    {
+
+
+            public TCard() { }
+        
+
+            public string Image { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime EventDate { get; set; }
+
+            public bool Voted { get; set; }
+
+            public bool Liked { get; set; }
+
+            public string Hashtags { get; set; }
+
+
+
+
+        
+    }
+}
+
---
+++ Web/Shared/TcardManip.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Data;
+
+
+namespace Web.Shared
+{
+    public class TcardManip
+    {
+        public TcardManip()
+        {
+        }
+
+        private static readonly DateTime[] EventDates = new[]
+        {
+            new DateTime(2010, 10, 1),
+            new DateTime(2010, 10, 2),
+            new DateTime(1995, 6, 1),
+            new DateTime(1994, 2, 1),
+        };
+
+        private static readonly string[] Names = new[]
+        {
+            "Kitchen Service", "Cleanup", "Community Service", "Eating Ass",
+        };
+
+        private static readonly string[] Hashtagsx = new[]
+        {
+             "BLM", "Cooking", "Service", "Art",
+        };
+
+        
+
+         TCard Info1 = new TCard()
+         {
+            EventDate = new DateTime(2008, 3, 15), Name = "Kitchen Service", Hashtags = "BLM",
+            
+         };
+
+        
+        
+    
+ 
+        public async Task<TCard[]> GetInfoAsync()
+        {
+            var rng = new Random();
+
+            return await Task.FromResult(Enumerable.Range(1, 5).Select(index => new TCard
+            {
+                EventDate = EventDates[rng.Next(EventDates.Length)],
+                Name = Names[rng.Next(Names.Length)],
+                Hashtags = Hashtagsx[rng.Next(Hashtagsx.Length)]
+
+
+            }).ToArray());
+        }
+
+        
+    }
+}

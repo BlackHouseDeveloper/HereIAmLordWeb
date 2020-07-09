70375ad6305ee79c88e761607b5e2848cf3f0270
Cartercoder <calvin.carter35@gmail.com>
2020-07-09 13:55:16 -0400

    fix

---
+++ Web/Data/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Identity;
+
+namespace HereIAmAPI.Model
+{
+    public class Profile : IdentityUser
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required, StringLength(20)]
+        public string FName { get; set; }
+
+        [Required, StringLength(20)]
+        public string LName { get; set; }
+
+        [Required]
+        public DateTime Bday { get; set; }
+
+        
+
+        [Required, StringLength(500)]
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
+
+        public string UserId { get; set; }
+
+        
+
+
+    }
+}
---
+++ Web/Data/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HereIAmAPI.Model
+{
+    public class ProfileViewModel
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public DateTime Bday { get; set; }
+        public string ImagePath { get; set; }
+
+        
+    }
+}
